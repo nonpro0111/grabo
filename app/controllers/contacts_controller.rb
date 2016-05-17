783603c753726f3a889b0457bf2d54273ba9b4a7
nonpro <nonprofessional0111@gmail.com>
2016-05-17 23:54:03 +0900

    お問い合わせscaffold

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,38 @@
+class ContactsController < ApplicationController
+
+  # GET /contacts/new
+  def new
+    @contact = Contact.new
+  end
+
+  # POST /contacts
+  # POST /contacts.json
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def finish
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def contact_params
+      params.require(:contact).permit(:email, :title, :body)
+    end
+end
