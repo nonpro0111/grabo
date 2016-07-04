db28d148cfe3c3c034ca83a98ba33fae901972a8
nonpro <nonprofessional0111@gmail.com>
2016-07-05 00:05:55 +0900

    sign in/out 後のリダイレクト先変更

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,5 +1,5 @@
 Admin::Engine.routes.draw do
-  devise_for :users, class_name: "Admin::User", module: :devise
+  devise_for :users, class_name: "Admin::User", module: :devise, controllers: {sessions: "admin/sessions"}
   resources :videos
   resources :home, only: :index
 
