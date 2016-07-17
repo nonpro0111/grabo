627cebcb0778fb1be4eaf9ef75b123e9dfbab542
nonpro <nonprofessional0111@gmail.com>
2016-07-17 21:35:00 +0900

    広告設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :sidebar
+  before_action :sidebar, :lower_ad
 
   def sidebar
     @popular_videos = Video.popular
@@ -10,4 +10,7 @@ class ApplicationController < ActionController::Base
     @sidebar_ads = Advertisement.sidebar
   end
 
+  def lower_ad
+    @lower_ads = Advertisement.lower
+  end
 end
