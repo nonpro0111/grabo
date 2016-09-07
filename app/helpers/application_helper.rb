1c1f67ac0724e9157659cd1d3dff9aca8f0f59d6
nonpro <nonprofessional0111@gmail.com>
2016-09-08 02:23:26 +0900

    google search consoleの指摘対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+
+  def meta_tag_title
+    title = ""
+    num = Random.rand(1..3)
+    words = Global.meta_data.words.sample(num)
+
+    words.each_with_index do |word, i|
+      title << word
+      if i == (num - 1)
+        title << "グラビア動画"
+      else
+        title << "、"
+      end
+    end
+
+    title
+  end
+
+  def meta_tag_description(word = nil)
+    if word
+      <<-"EOS"
+        人気グラビアアイドルのおすすめ動画をまとめているグラビア動画BOX。
+        #{word}動画たくさん取り揃えています。
+      EOS
+    else
+      words = Global.meta_data.words.sample(5)
+      <<-"EOS"
+        人気グラビアアイドルのおすすめ動画をまとめているグラビア動画BOX。
+        #{words[0]}、#{words[1]}、#{words[2]}アイドルや#{words[3]}、#{words[4]}
+        アイドルなど幅広く掲載しています。
+      EOS
+    end
+  end
 end
