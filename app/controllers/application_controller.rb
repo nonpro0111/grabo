b64eecf76845149d9b6d056cfe7ae14b23d86e4c
nonpro <nonprofessional0111@gmail.com>
2016-09-21 00:23:57 +0900

    affiliate変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :sidebar, :lower_ad
+  helper_method :request_smartphone?
+  helper_method :request_pc?
 
   def sidebar
     @popular_videos = Video.popular
@@ -12,5 +14,17 @@ class ApplicationController < ActionController::Base
 
   def lower_ad
     @lower_ads = Advertisement.lower
+  end
+
+  def request_smartphone?
+    begin
+      request.env["HTTP_USER_AGENT"].include?("Mobile")
+    rescue
+      false
+    end
+  end
+
+  def request_pc?
+    !request_smartphone?
   end
 end
--- app/views/videos/feed.rss.builder
+++ app/views/videos/feed.rss.builder
@@ -5,6 +5,7 @@ xml.rss("version" => "2.0", "xmlns:dc" => "http://purl.org/dc/elements/1.1/") do
  
     @videos.each do |video|
       xml.item do
+        xml.category video.tag_list.first
         xml.title video.title
         xml.description video.description
         xml.pubDate video.created_at.to_s(:rfc822)
